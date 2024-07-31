ab84ac5cb54b3e7e942055cc89cc695fd522ba73
Tan Shin Jie <shinjie.tan@alta.exchange>
2024-07-31 09:41:23 +0800

    fix: No chromium

--- source/scrape.py
+++ source/scrape.py
@@ -4,15 +4,19 @@ from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-executable_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-chrome_service = Service(executable_path=executable_path)
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+# executable_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+# chrome_service = Service(executable_path=executable_path)
 
 chrome_options = Options()
 options = [
     "--headless",
     "--disable-gpu",
     "--window-size=1920,1200",
-    "--ignore-certificate-errors",
+    "--ignore-certificate-errors",j
     "--disable-extensions",
     "--no-sandbox",
     "--disable-dev-shm-usage"
@@ -20,7 +24,9 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('http://nytimes.com')
 print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
